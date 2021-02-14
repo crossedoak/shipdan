d0691d1ef9f1342988992fd1dc238afcea2b576b
crossedOak <crossedoak@gmail.com>
2021-02-14 02:03:10 -0500

    database is now functioning

--- shipdan.py
+++ shipdan.py
@@ -62,6 +62,10 @@ for result in results['matches']:
 	outputList.append(rawTitle + ' - ' + result['ip_str'] + '\nSeen: ' + result['timestamp'] + '\n')
 	
 #Shodan search 2
+unitList = []
+titleList = []
+shipIPList = []
+shipTimestampList = []
 try:
 	results = api.search('title:"SAILOR" port:8080')
 	totalSearch2 = '{}'.format(results['total'])
@@ -73,30 +77,145 @@ for result in results['matches']:
 		page = BeautifulSoup(requests.get('http://' + IPlist + ':8080').text, "html.parser")
 		rawTitle = page.find(id="status_hostname").text
 		title = rawTitle.split(" - ")[0]
+		unit = rawTitle.split(" - ")[1]
 		outputList.append(rawTitle + ' - ' + result['ip_str'] + '\nSeen: ' + result['timestamp'] + '.\n')
+		unitList.append(unit)
+		titleList.append(title)
+		shipIPList.append(IPlist)
+		shipTimestampList.append(result['timestamp'])
 	except:
 		print ('Error connecting to host: {}'.format(result['ip_str']))
 
 
 #Shodan search 3
 try:
+	fleetBroadbandListIP = []
+	fleetBroadbandListTimestamp = []
 	results = api.search('title:"Thrane &amp;"')
 	totalFleetBroadband = '{}'.format(results['total'])
+	for result in results['matches']:
+		fleetBroadbandListIP.append(result['ip_str'])
+		fleetBroadbandListTimestamp.append(result['timestamp'])
+	
+		
+		
 except shodan.APIError as e:
 		print ('Error: {}'.format(e))
 
+aptusWebListIP = []
+aptusWebListTimestamp = []
 try:
  	#searching
 	results = api.search('title:"Intellian Aptus Web"')
 	totalAptusWeb = '{}'.format(results['total'])
+	for result in results['matches']:
+		aptusWebListIP.append(result['ip_str'])
+		aptusWebListTimestamp.append(result['timestamp'])
+
 except shodan.APIError as e:
 		print ('Error: {}'.format(e))
-
+print(aptusWebListIP)
 
 #refine the list
 refinedoutputList = list(set(outputList))
 
+#database things start here
+def create_connection(db_file):
+    conn = None
+    print(db_file)
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+        return conn
+    except Error as e:
+    	print("made it to the error of create connection " + str(e))
+    return conn
 
+
+def create_table(conn, create_table_sql):
+	try:
+		c = conn.cursor()
+		c.execute(create_table_sql)
+	except Error as e:
+		print(e)
+
+def create_fleetBroadband(conn, FleetBroadband):
+	sql = '''INSERT OR IGNORE INTO FleetBroadband(id,ip,timestamp)
+			 VALUES(?,?,?) '''
+	cur = conn.cursor()
+	cur.execute(sql, FleetBroadband)
+	conn.commit()
+	return cur.lastrowid
+
+def create_Ships(conn, ship):
+	sql = '''INSERT OR IGNORE INTO ships(id,ip,unit,name,timestamp)
+			 VALUES(?,?,?,?,?) '''
+	cur = conn.cursor()
+	cur.execute(sql, ship)
+	conn.commit()
+	return cur.lastrowid
+
+def create_AptusWeb(conn, AptusWeb):
+	sql = '''INSERT OR IGNORE INTO AptusWeb(id,ip,timestamp)
+			 VALUES(?,?,?) '''
+	cur = conn.cursor()
+	cur.execute(sql, AptusWeb)
+	conn.commit()
+	return cur.lastrowid
+
+
+def main():
+	database = r"/root/Documents/shipdan/shipdan.db"
+
+	sql_create_ships_table = """ CREATE TABLE IF NOT EXISTS ships (
+									id int,
+									ip text PRIMARY KEY,
+									unit text,
+									name text NOT NULL,
+									timestamp text
+									); """
+
+	sql_create_FleetBroadband = """CREATE TABLE IF NOT EXISTS FleetBroadband (
+									id int,
+									ip text PRIMARY KEY,
+									timestamp text
+									); """
+	sql_create_AptusWeb = """CREATE TABLE IF NOT EXISTS AptusWeb (
+									id int,
+									ip text PRIMARY KEY,
+									timestamp text
+									); """
+
+	conn = create_connection(database)
+
+	if conn is not None:
+		create_table(conn, sql_create_ships_table)
+		create_table(conn, sql_create_FleetBroadband)
+		create_table(conn, sql_create_AptusWeb)
+	else:
+		print("error! cannot create the database connection")
+
+	with conn:
+		count = 0
+		for item, item2 in list(zip(fleetBroadbandListIP, fleetBroadbandListTimestamp)):
+			count += 1
+			fleetBroadband = (count, item, item2)
+			fleetBroadband_id = create_fleetBroadband(conn, fleetBroadband)
+		for item, item2, item3, item4 in list(zip(shipIPList, unitList, titleList, shipTimestampList)):
+			count += 1
+			ships = (count, item, item2, item3, item4)
+			ships_id = create_Ships(conn, ships)
+		for item, item2 in list(zip(aptusWebListIP, aptusWebListTimestamp)):
+			count += 1
+			aptusWeb = (count, item, item2)
+			aptusWeb_id = create_AptusWeb(conn, aptusWeb)
+
+if __name__ == '__main__':
+	main()
+
+
+
+'''
 #auth to twitter
 auth = tweepy.OAuthHandler(consumerKey,  consumerSecret)
 auth.set_access_token(accessToken, secretToken)
@@ -111,11 +230,6 @@ except:
 total = int(totalFleetBroadband) + int(totalAptusWeb) + int(totalSearch1) + int(totalSearch2)
 tweetHeader = str(total) + " vessel terminals found on Shodan. \n" + totalFleetBroadband + " Thrane Fleet Broadband\n" + totalAptusWeb + " Intellian AptusWeb portals\n" 
 print( tweetHeader + '\n' + '\n'.join([str(i) for i in refinedoutputList]))
-
-
-
-
-
 
 userChoice = input('\nIs this what you want to tweet? (Y/N): ')
 if userChoice.lower() == 'y' or userChoice.lower() == 'yes': 
@@ -134,60 +248,17 @@ if userChoice.lower() == 'y' or userChoice.lower() == 'yes':
 			preTweet = preTweet + word
 	tweet_list.append(preTweet)
 	for post in tweet_list:
-		api.update_status(post)
+		#api.update_status(post)
+		print("Tweeted Successfully")
 	
-	print("Tweeted successfully")
+	
 elif userChoice.lower() == 'n' or userChoice.lower() == 'no':
 	print("No tweet created. Check inputs.")
 	exit(0)
 else: 
 	print("Invalid choice, exiting")
 	exit(0)
+'''
 
-'''#database things start here
-def create_connection(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-        return conn
-    except Error as e:
-        print(e)
-    return conn
 
-def create_table(conn, create_table_sql):
-	try:
-		c = conn.cursor()
-		c.execute(create_table_sql)
-	except Error as e:
-		print(e)
 
-def main():
-	database = r"~/Documents/shidan/shipdan.db"
-
-	sql_create_ships_table = """ CREATE TABLE IF NOT EXISTS ships (
-									id integer PRIMARY KEY,
-									name text NOT NULL,
-									ip text NOT NULL,
-									timestamp text
-									); """
-
-	sql_create_FleetBroadband = """CREATE TABLE IF NOT EXISTS FleetBroadband (
-									id integer PRIMARY KEY,
-									ip text NOT NULL,
-									timestamp text
-									); """
-
-	conn = create_connection(database)
-
-	if conn is not None:
-		create_table(conn, sql_create_ships_table)
-		create_table(conn, sql_create_FleetBroadband)
-	else:
-		print("error! cannot create the database connection")
-
-if __name__ == '__main__':
-	#create_connection(r"~/Documents/shidan/shipdan.db")
-	main()
-
-	'''
